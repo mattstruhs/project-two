a256bdcf0052d41a44f4dd7664c66300b0b8c680
Matt S <mattstruhs@yahoo.com>
2021-04-10 17:23:11 -0400

    adding params for api

--- routes/gws.route.js
+++ routes/gws.route.js
@@ -11,11 +11,29 @@ const axios = require("axios");
 
 router.get("/wines", (req, res, next) => {
   console.log("great job on the post route!");
-  let countryName = req.query.country;
+  let limitResults = numResults;
+  let vintage = req.query.vintage
+  let color = req.query.color
+  let region = req.query.region
+  let country = req.query.country
+  let score = req.query.score
+  
+  
+  // ?wine_id=
+  // &vintage=2000
+  // &color=
+  // &is_primeurs=&
+  // lwin=1014033&
+  // lwin_11=10140332000&
+  // limit=100&
+  // offset=100&
+  // ordering=-date
+
   console.log(countryName);
   axios
     .get(
-      `https://api.globalwinescore.com/globalwinescores/latest/?country=${countryName}`,
+      `https://api.globalwinescore.com/globalwinescores/latest/?country=${country}&
+      `,
       {
         headers: {
           Accept: "application/json",
