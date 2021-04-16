201c07b19cb72d3bd48c2d3791084b77eeeb8f95
Matt S <mattstruhs@yahoo.com>
2021-04-15 21:18:37 -0400

    protected routes working

--- config/user-check.config.js
+++ config/user-check.config.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
     if (!req.session.user) {
-      res.redirect("/login");
+      res.redirect("/auth/login");
     }
     next();
   };
--- routes/journal.route.js
+++ routes/journal.route.js
@@ -2,11 +2,10 @@ const express = require("express");
 const router = express.Router();
 const Journal = require("../models/Journal.model");
 // const axios = require("axios");
-// const userCheck = require("../config/user-check.config");
+const userCheck = require("../config/user-check.config");
 
-router.get("/journal", (req, res, next) => {
+router.get("/journal", userCheck, (req, res, next) => {
   // get all the entries from DB
-  console.log("your at the journal index page", req.query);
   res.render("journal/index");
 //   Journal.find({})
 //     .then((journalFromDB) => {
