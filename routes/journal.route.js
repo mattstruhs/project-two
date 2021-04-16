1731a7d261a49dcf7726d91ac205d9e75972e270
Matt S <mattstruhs@yahoo.com>
2021-04-15 22:01:11 -0400

    working on hidden inputs in wine cards

--- routes/journal.route.js
+++ routes/journal.route.js
@@ -6,12 +6,32 @@ const userCheck = require("../config/user-check.config");
 
 router.get("/journal", userCheck, (req, res, next) => {
   // get all the entries from DB
-  res.render("journal/index");
-//   Journal.find({})
-//     .then((journalFromDB) => {
-//       res.render("journal/index.hbs", { journalFromDB });
-//     })
-//     .catch((err) => next(err));
+  Journal.find({})
+    .then((journalFromDB) => {
+      res.render("journal/index.hbs", { journalFromDB });
+    })
+    .catch((err) => next(err));
+});
+
+router.post("/journal", userCheck, (req, res, next) => {
+  // get info from api about wine
+  const {
+    wine_id,
+    wine,
+    vintage,
+    appellation,
+    color,
+    regions,
+    country,
+    score,
+    writeUp,
+  } = req.body;
+  console.log(req.body)
+  Journal.create(req.body)
+    .then(() => {
+      res.render("journal/index.hbs");
+    })
+    .catch((err) => next(err));
 });
 
 module.exports = router;
