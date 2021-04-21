e2c6ddcae1761ab480cc9308dae46a8f6e4731d2
nottus <mcelroy.sc@gmail.com>
2021-04-20 18:17:40 -0700

    journal partial

---
+++ routes/profile.route.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+const Journal = require("../models/Journal.model");
+// const axios = require("axios");
+const userCheck = require("../config/user-check.config");
+
