67bc8be7e2cdcacc73d42c90c4cec4c9a97bbab1
Matt Struhs <mattstruhs@yahoo.com>
2021-04-22 06:55:42 -0400

    added some comments to gws.route

--- routes/gws.route.js
+++ routes/gws.route.js
@@ -5,11 +5,15 @@ const { response } = require("express");
 const SavedResultsFromAPI = require("../models/ApiResults.model");
 const completedSearches = require("../models/CompletedSearches.model");
 
-// set up async to avoid all of the nested then
+// set up async to avoid all of the nested .then
+// each operations must complete before moving on to the next
 router.get("/wines", async (req, res, next) => {
-  console.log("great job on the post route!");
+  console.log(
+    "Got your search paramaters! Let's see if we have done this search before."
+  );
   const { limit, color, country } = req.query;
 
+  // check to see if we have done this search before
   const previousSearchResultsFromDB = await completedSearches
     // timestamp was in past 3 days to stay current
     // endpoint check if data is current
@@ -22,9 +26,12 @@ router.get("/wines", async (req, res, next) => {
     .populate("wines");
 
   if (previousSearchResultsFromDB) {
-    console.log("these results are from Mongo");
+    console.log(
+      "We've already saved these results from a previous search! Here are the results from Mongo"
+    );
     res.render("wines", { wineInfo1: previousSearchResultsFromDB.wines });
   } else {
+    console.log("No luck on finding the results in our DB. Making the request from the api now!")
     const responseFromAPI = await axios.get(
       `https://api.globalwinescore.com/globalwinescores/latest/?country=${country}&color=${color}&limit=${limit}`,
       {
@@ -35,16 +42,23 @@ router.get("/wines", async (req, res, next) => {
       }
     );
     const wineInfo1 = responseFromAPI.data.results;
+
+    console.log("We got the results back from the api request! Storing them now for future reference!")
     const createdWines = await SavedResultsFromAPI.create(wineInfo1);
 
+    // store just the wine ids so we can store them in our previous searches model
     const wines = createdWines.map((wine) => wine._id);
+
+    // save the search paramaters so next time we can see if this search has been done before
+    // we use the list of wine ids (wines) to link between previous searches and our full results from api
+   console.log("Storing the search parameters from our request so we can keep track of our searches")
     const savedRes = await completedSearches.create({
       limit,
       color,
       country,
       wines,
     });
-    console.log({ savedRes });
+    console.log("these are the results we saved",  savedRes );
     res.render("wines", { wineInfo1 });
   }
 });
