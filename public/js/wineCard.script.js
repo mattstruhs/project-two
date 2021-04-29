44373b63b49e80a4835281bb6cafe429903f98c9
Matt S <mattstruhs@yahoo.com>
2021-04-29 18:16:52 -0400

    created DOM script for wineCard

---
+++ public/js/wineCard.script.js
@@ -0,0 +1,5 @@
+divLoad = document.getElementById("already-in-my-collection");
+wineCard = document.getElementById("wine-card");
+if (!divLoad) {
+  console.log("we found a wine already in your journal", wineCard);
+}
